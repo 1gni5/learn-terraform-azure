993d227b71c3a408af8710fac0026737080be70f
1gni5 <jules.ducange@posteo.net>
2026-05-27 11:27:35 +0200

    Add output for virtual network address space in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "virtual_network_name" {
   value = azurerm_virtual_network.vnet.name
 }
+
+output "virtual_network_address_space" {
+  value = azurerm_virtual_network.vnet.address_space
+}
